7755c20581df4ffbdab72e534514b4049679487a
bradenfrazer <bradenfrazer@gmail.com>
2022-02-17 12:42:02 -0600

    move calculating result into store

--- src/WordRow.tsx
+++ src/WordRow.tsx
@@ -1,45 +1,42 @@
-import { useStore } from "./store"
-import { computeGuess, LetterState, LETTER_LENGTH } from "./word-utils"
+import { LetterState, LETTER_LENGTH } from "./word-utils"
 
 interface WordRowProps {
-    letters: string
+  letters: string,
+  result?: LetterState[]
 }
 
-export default function WordRow({letters: lettersProp = ''}: WordRowProps) {
-    const answer = useStore(state => state.answer)
-    const lettersRemaining = LETTER_LENGTH - lettersProp.length
-    const letters = lettersProp.split('').concat(Array(lettersRemaining).fill(''))
+export default function WordRow({letters: lettersProp = '', result = []}: WordRowProps) {
+  const lettersRemaining = LETTER_LENGTH - lettersProp.length
+  const letters = lettersProp.split('').concat(Array(lettersRemaining).fill(''))
+  
+  return (
+    <div className='grid grid-cols-5 gap-4'>
+      { letters.map((char, index) => (
+          <CharacterBox key={index} value={char} state={result[index]} />
+      ) ) }
+    </div> 
+  )
+}
 
-    const guessStates = computeGuess(lettersProp, answer)
-    
-    return (
-      <div className='grid grid-cols-5 gap-4'>
-        { letters.map((char, index) => (
-            <CharacterBox key={index} value={char} state={guessStates[index]} />
-        ) ) }
-      </div> 
-    )
-  }
+interface CharacterBoxProps {
+    value: string
+    state?: LetterState
+}
 
-  interface CharacterBoxProps {
-      value: string
-      state?: LetterState
-  }
+function CharacterBox({ value, state }: CharacterBoxProps) {
+  const stateStyles = state == null ? '' : characterStateStyles[state]
 
-  function CharacterBox({ value, state }: CharacterBoxProps) {
-    const stateStyles = state == null ? '' : characterStateStyles[state]
+  return (
+    <span className={`inline-block border-2 border-gray-500 p-4 
+    before:inline-block before:content-['_'] 
+    uppercase font-bold text-2xl text-center ${stateStyles}`}>
+        {value}
+    </span>
+  )
+}
 
-    return (
-      <span className={`inline-block border-2 border-gray-500 p-4 
-      before:inline-block before:content-['_'] 
-      uppercase font-bold text-2xl text-center ${stateStyles}`}>
-          {value}
-      </span>
-    )
-  }
-
-  const characterStateStyles = {
-    [LetterState.Miss]: 'bg-gray-500 border-gray-500',
-    [LetterState.Present]: 'bg-yellow-500 border-yellow-500',
-    [LetterState.Match]: 'bg-green-500 border-green-500'
-  }
+const characterStateStyles = {
+  [LetterState.Miss]: 'bg-gray-500 border-gray-500',
+  [LetterState.Present]: 'bg-yellow-500 border-yellow-500',
+  [LetterState.Match]: 'bg-green-500 border-green-500'
+}
