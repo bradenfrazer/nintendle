c9ecb6d991466040f0fffd950388e074cac301b2
bradenfrazer <bradenfrazer@gmail.com>
2022-02-21 16:19:18 -0600

    properly type modal props

--- src/Modal.tsx
+++ src/Modal.tsx
@@ -1,7 +1,7 @@
 import CrossIcon from '../assets/cross.svg'
 
-export default function About(props: any) {
-  if (!props.show) return null
+export default function Modal({show, onClose, children} : {show: boolean, onClose: () => void, children: JSX.Element}) {
+  if (!show) return null
 
   return ( 
     <div role='modal' 
@@ -10,12 +10,12 @@ export default function About(props: any) {
       w-full p-6 m-3/4 mx-auto
       sm:w-3/4 lg:w-1/2'>
           <div className='relative'>
-            <button className='absolute right-0' onClick={ props.onClose }>
-              <img className='w-4' src={CrossIcon} />
+            <button className='absolute right-0' onClick={ onClose }>
+              <img className='w-4' src={ CrossIcon } />
             </button>
           </div>
           <div>
-            {props.children}
+            { children }
           </div>
         </div>
     </div>
